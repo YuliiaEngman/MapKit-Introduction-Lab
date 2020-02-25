e4c60101eb62a9c53d9ed4c682723a754ef58187
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-25 12:38:44 -0500

    completed CoreLocationSession file

--- MapKit-Introduction-Lab/CoreLocationSession.swift
+++ MapKit-Introduction-Lab/CoreLocationSession.swift
@@ -23,5 +23,42 @@ class CoreLocationSession: NSObject {
 }
 
 extension CoreLocationSession: CLLocationManagerDelegate {
+    //this methos is called when user changes the location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("didUpdateLocations") //gives us back an array of CLLocations (ususally we need the last one - most updated)
+    }
     
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError: \(error)")
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // gives us status
+        switch status {
+        case .authorizedAlways:
+            print("authorizedAlways") //purpose is to see state of authorization
+        case .authorizedWhenInUse:
+            print("authorizedWhenInUse")
+        case .denied:
+            print("denied")
+        case .notDetermined:
+            print("notDetermined")
+        case .restricted:
+            print("restricted")
+            
+        //we need to add default case since apple may add another cases:
+        default:
+            break
+        }
+    }
+    
+    //think about is as a circle (region has a center and radius)
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("didEnterRegion")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("didExitRegion")
+    }
 }
