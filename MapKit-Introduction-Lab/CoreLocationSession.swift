7cce0dadc22305935873c97a5bc4e8cd8accc70a
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-25 18:41:37 -0500

    created functions to get coordinates from place name and wise versa

--- MapKit-Introduction-Lab/CoreLocationSession.swift
+++ MapKit-Introduction-Lab/CoreLocationSession.swift
@@ -9,6 +9,22 @@
 import Foundation
 import CoreLocation
 
+struct Location {
+    let title: String
+    let body: String
+    let coordinate: CLLocationCoordinate2D
+    let imageName: String
+    
+    // this is Model -> In the lab I shpuld use JSON
+    static func getLocations() -> [Location] {
+        return [
+            Location(title: "Pursuit", body: "We train adults with the most need and potential to get hired in tech, advance in their careers, and become the next generation of leaders in tech.", coordinate: CLLocationCoordinate2D(latitude: 40.74296, longitude: -73.94411), imageName: "team-6-3"),
+            Location(title: "Brooklyn Museum", body: "The Brooklyn Museum is an art museum located in the New York City borough of Brooklyn. At 560,000 square feet (52,000 m2), the museum is New York City's third largest in physical size and holds an art collection with roughly 1.5 million works", coordinate: CLLocationCoordinate2D(latitude: 40.6712062, longitude: -73.9658193), imageName: "brooklyn-museum"),
+            Location(title: "Central Park", body: "Central Park is an urban park in Manhattan, New York City, located between the Upper West Side and the Upper East Side. It is the fifth-largest park in New York City by area, covering 843 acres (3.41 km2). Central Park is the most visited urban park in the United States, with an estimated 37.5–38 million visitors annually, as well as one of the most filmed locations in the world.", coordinate: CLLocationCoordinate2D(latitude: 40.7828647, longitude: -73.9675438), imageName: "central-park")
+        ]
+    }
+}
+
 class CoreLocationSession: NSObject {
     
     public var locationManager: CLLocationManager //dont need ! since we setting locationManager with a value (locationManager = CLLocationManager())
@@ -31,6 +47,36 @@ class CoreLocationSession: NSObject {
         
         //get updates for user location
         locationManager.startUpdatingLocation()
+    }
+    
+    // 2D has latitude and longitude (2 values)
+    public func convertCoordinateToPlacemark(coordinate: CLLocationCoordinate2D) {
+        // we will use the CLGeocoder() class for converting coordinate (CLLocationCoordinate2D) to placemark (CLPlacemark)
+        
+        //we need to create CLLocation
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks,
+            error) in
+            if let error = error {
+                print("reverseGeocodeLocation: \(error)")
+            }
+            if let firstPlacemark = placemarks?.first {
+                print("placemark info: \(firstPlacemark)")
+            }
+        }
+    }
+    
+    public func convertPlaceNameToCoordinate(addressString: String) {
+        // converting an address to a coordinate
+        CLGeocoder().geocodeAddressString(addressString) { (placemarks, error) in
+            if let error = error {
+                print("geocodeAddressString: \(error)")
+            }
+            if let firstPlacemark = placemarks?.first,
+                let location = firstPlacemark.location {
+                print("coordinate is \(location.coordinate)")
+            }
+        }
     }
 }
 
--- MapKit-Introduction-Lab/ViewController.swift
+++ MapKit-Introduction-Lab/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  MapKit-Introduction-Lab
 //
-//  Created by Yuliia Engman on 2/24/20.
+//  Created by Yuliia Engman on 2/25/20.
 //  Copyright © 2020 Yuliia Engman. All rights reserved.
 //
 
@@ -14,7 +14,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //testing converting coordinate to placemark
+        convertCoordinateToPlacemark()
     }
+    
+    private func convertCoordinateToPlacemark() {
+          let location = Location.getLocations()[2]
+        //gives us Central Park: Central Park, Central Park, West Dr, New York, NY  10028, United States
+              locationSession.convertCoordinateToPlacemark(coordinate: location.coordinate)
+      }
+    
+//    private func convertCoordinateToPlacemark() {
+//        if let location = Location.getLocations().first {
+//            locationSession.convertCoordinateToPlacemark(coordinate: location.coordinate)
+//        }
+//    }
+    //Getting Pursuit location: placemark info: Skillman Ave, Skillman Ave, Long Island City, NY  11101, United States
 
 }
-
