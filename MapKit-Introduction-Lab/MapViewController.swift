166569dce5d68508e711ff888d6aa70944acb6e9
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-25 20:07:26 -0500

    all annotations are set

--- MapKit-Introduction-Lab/MapViewController.swift
+++ MapKit-Introduction-Lab/MapViewController.swift
@@ -27,9 +27,12 @@ class MapViewController: UIViewController {
         // configure map view
         // attempt to show the user's current location
         mapView.showsUserLocation = true
+        mapView.delegate = self
+        
+        loadMapView()
     }
     
-    func makeAnnotations() {
+    private func makeAnnotations() -> [MKPointAnnotation] {
         var annotations = [MKPointAnnotation]()
         for location in Location.getLocations() {
             let annotation = MKPointAnnotation()
@@ -37,6 +40,15 @@ class MapViewController: UIViewController {
             annotation.title = location.title
             annotations.append(annotation)
         }
+        return annotations
+    }
+    
+    private func loadMapView() {
+        let annotations = makeAnnotations()
+        //we can add 1 annotation or many annotations (using bult-in func)ann
+        mapView.addAnnotations(annotations)
+        
+        mapView.showAnnotations(annotations, animated: true)
     }
     
     private func convertCoordinateToPlacemark() {
@@ -55,5 +67,33 @@ class MapViewController: UIViewController {
     private func convertPlaceNameToCoordinate() {
         locationSession.convertPlaceNameToCoordinate(addressString: "Brooklyn Museum")
     }
+}
 
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("didSelect")
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else {
+            return nil
+        }
+        let identifier = "locationAnnotation"
+        var annotationView: MKPinAnnotationView
+        
+        // try to dequeue and reuse annotation view
+        // if it excist just assign to our annotation
+        if let dequeueView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+        annotationView = dequeueView
+    } else {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView.canShowCallout = true
+    }
+    return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        //bubble around annotation
+        print("calloutAccessoryControlTapped ")
+    }
 }
